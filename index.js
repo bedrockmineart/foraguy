81b5e60e3f1cb806e7ad59e2ea973df7b061d3d0
bedrockmineart <38589560+bedrockmineart@users.noreply.github.com>
2020-03-21 14:12:32 +0000

    Y

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let roblox = require('noblox.js');
 const bot = new Discord.Client();
 const fs = require("fs");
 bot.msgs = require ("./msgs.json")
-const Canvas = require('canvas')
 
 
 
@@ -67,17 +66,6 @@ bot.on('ready', () => {
     console.log('Hello 1 2 1 2, Sub bot is online')
     console.log('We are online')
 })
-
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
-	let fontSize = 70;
-
-	do {
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	return ctx.font;
-};
 
 bot.on('ready', () => {
   bot.channels.get("681248976590209034").fetchMessage("681540364926320644").then((message) => {
@@ -216,41 +204,6 @@ bot.on('guildMemberAdd', member => {
   let memberCountChannel = myGuild.channels.get('681249520251699214');
   memberCountChannel.setName('Members: ' + membercount)
   .catch(error => console.log(error));
-  
-});
-
-bot.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'welcome');
-	if (!channel) return;
-
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('./wallpaper.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-
-	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 bot.on('guildMemberRemove', member => {
